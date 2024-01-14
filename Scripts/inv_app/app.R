a23abe3d618ee100fd45dba40082bb9173affad7
m1sterjay <95725449+m1sterjay@users.noreply.github.com>
2024-01-14 15:19:36 -0500

    More functionality and stlying in the app. Ready for deployment.

--- Scripts/inv_app/app.R
+++ Scripts/inv_app/app.R
@@ -3,8 +3,8 @@
 #
 # This app scans a list of species for potential invasiveness using the GISD database.
 # Creator: Johannes Nelson https://github.com/johannesnelson
-# Credit: I created this app by modifiyng existing functions within a defunct
-# package called 'originr' (https://github.com/ropensci-archive/originr)
+# Credit: I created this app with inspiration from an existing function in an old
+# package called 'originr' (https://github.com/ropensci-archive/originr).
 # --------------------------------------------------------------------------------
 
 library(jsonlite)
@@ -16,7 +16,7 @@ library(stringr)
 
 
 # Define primary function that checks invasiveness
-check_invasive_status <- function (file_path, ...) 
+check_invasive_status <- function (file_path = NULL, species_list = NULL, ...) 
 {
   
   # Define helper function that checks GBIF first
@@ -32,15 +32,23 @@ check_invasive_status <- function (file_path, ...)
   }
   
   
-  # Load in data
-  species_file <- read.csv(file_path)
-  if (!"species" %in% names(species_file)) {
-    print("There is no column names 'species' in this file. Please rename and try again.")
-    return(NULL)
-  }
-  # Extract species names from data
-  species_to_check <- species_file$species
-  total_species <- length(species_to_check)
+
+    # Decide whether to use file input or direct input
+    if (!is.null(species_list)) {
+      species_to_check <- species_list
+    } else if (!is.null(file_path)) {
+      species_file <- read.csv(file_path)
+      if (!"species" %in% names(species_file)) {
+        print("There is no column named 'species' in this file. Please rename and try again.")
+        return(NULL)
+      }
+      species_to_check <- species_file$species
+    } else {
+      print("No species data provided.")
+      return(NULL)
+    }
+    
+    total_species <- length(species_to_check)
   results <- list()
   
   # Loop through names and check for invasiveness
@@ -106,13 +114,14 @@ check_invasive_status <- function (file_path, ...)
       species = x$species,
       alien_range = paste(x$alien_range, collapse = ", "),
       native_range = paste(x$native_range, collapse = ", "),
-      summary = x$summary
+      summary = x$summary,
+      status = "Invasive"
     )
   })
   all_invasives_df <- do.call(rbind, invasive_dfs)
   
   final_df <- bind_rows(all_invasives_df, not_invasive_df)
-  final_df <- final_df %>% mutate(status = ifelse(is.na(status), "Invasive", status))
+  # final_df <- final_df %>% mutate(status = ifelse(is.na(status), "Invasive", status))
   
   # date_info <- format(Sys.time(), "%Y-%m-%d")
   # file_name <- paste0("Invasive_Species_Report_", date_info, ".csv")
@@ -130,17 +139,22 @@ ui <- fluidPage(
     tags$style(HTML("
       body {
         font-family: 'Helvetica', sans-serif;
-        background-color: #FFFFFF;
+        background-image: url('species_graphic.png');
+        background-position: center top;
+        background-size: cover; 
+        background-repeat: no-repeat;
+
       }
       .container {
         max-width: 800px;
         margin: 0 auto;
         padding: 20px;
-        background-color: #DEE3E1;
+        background-color: rgba(222, 227, 225, 0.97); 
         box-shadow: 0 0 10px rgba(0,0,0,0.1);
         border-radius: 5px;
         margin-top: 20px;
       }
+
       h1 {
         font-size: 24px;
         margin-bottom: 20px;
@@ -151,7 +165,22 @@ ui <- fluidPage(
         border: 1px solid #ccc;
         border-radius: 5px;
         padding: 10px;
+        background-color: rgba(255, 255, 255, .9); 
       }
+
+      .shiny-notification {
+              height: 100px;
+              width: 800px;
+              position:fixed;
+              top: calc(50% + 50px);;
+              left: calc(50% - 400px);
+              font-size: 20px;;
+      }
+      .custom-sidebar {
+      background-color: rgba(255, 255, 255, .8);
+      }
+      
+
     "))
   ),
   # Main container
@@ -159,19 +188,38 @@ ui <- fluidPage(
       titlePanel("Invasive Species Scanner"),
       sidebarLayout(
         sidebarPanel(
-          fileInput("file1", "Choose CSV File",
+          class = "custom-sidebar",
+          tags$strong("Run the app by choosing one of the three options below:"),
+          tags$br(),
+          
+          fileInput("file1", "1.) Choose CSV File",
                     accept = c(
                       "text/csv",
                       "text/comma-separated-values,text/plain",
                       ".csv")
           ),
-          actionButton("scan", "Scan for Invasiveness"),
+          tags$p(paste0("Note: If you are using your own CSV, make sure there is a column, ",
+                        "called 'species' that contains scientific names for the species you want to scan."), style = "font-size: 10px;"),
+          tags$br(),
+          textInput("speciesInput", "2.) Or Enter Species Names (comma-separated)"),
+          actionButton("scan", "Scan for Invasive Species"),
+          tags$p("Once you've loaded a CSV or entered species names, press the 'scan' button above.", style = "font-size: 10px;"),
+          tags$strong("3.) Or click the 'Run Demo' button below to demo the app using pre-loaded data"),
+          tags$br(),
+          actionButton("runDemo", "Run Demo", title = "Click to run a demonstration using example data"),
+          tags$br(),
+          tags$br(),
+
+          tags$p("Once scan is finished, you can download the results."),
           downloadButton("downloadResults", "Download Results")
         ),
         mainPanel(
+          tags$p("Results will appear here once the app is run."),
           div(class = "scrollable-table",  # Add scrollable div
               tableOutput("results")
           ),
+          # tags$img(src = "species_graphic.png"),
+          
           # Add this line for status text
           textOutput("statusText")
         )
@@ -182,13 +230,31 @@ server <- function(input, output) {
   results <- reactiveVal()
   
   observeEvent(input$scan, {
-    req(input$file1)
-    inFile <- input$file1
+    if (input$speciesInput != "") {
+      species_names <- unlist(strsplit(input$speciesInput, ",\\s*"))
+      withProgress(message = 'Scanning species for invasiveness', value = 0, {
+        results(check_invasive_status(species_list = species_names))
+      })
+    } else {
+      req(input$file1)
+      inFile <- input$file1
+      withProgress(message = 'Scanning species for invasiveness', value = 0, {
+        results(check_invasive_status(file_path = inFile$datapath))
+      })
+    }
     
-    withProgress(message = 'Scanning species for invasiveness', value = 0, {
-      results(check_invasive_status(inFile$datapath))
+    output$results <- renderTable({
+      results()
     })
-    
+  })
+  
+  observeEvent(input$runDemo, {
+    # Define the path to the example data file
+    example_file_path <- "example_data.csv" # Adjust the path if needed
+    print("Run Demo clicked")  # Debugging line
+    withProgress(message = 'Running demo with example data', value = 0, {
+      results(check_invasive_status(file_path = example_file_path))
+    })
     output$results <- renderTable({
       results()
     })
@@ -203,6 +269,9 @@ server <- function(input, output) {
     }
   )
 }
+
+shinyApp(ui = ui, server = server)
+
 
 
 shinyApp(ui = ui, server = server)
